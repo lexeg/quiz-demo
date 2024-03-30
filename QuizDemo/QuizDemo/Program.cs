320cb6c0ffbc1e8d1d5c46958062c36d3e616848
apetrenko <apetrenko@gseis.ru>
2024-03-30 21:33:53 +0500

    Добавил болванку для Web API

---
+++ QuizDemo/QuizDemo/Program.cs
@@ -0,0 +1,14 @@
+namespace QuizDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.development.json"))
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+}
---
+++ QuizDemo/QuizDemo/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace QuizDemo;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", () => "Hello World!");
+            endpoints.MapControllers();
+        });
+    }
+}
