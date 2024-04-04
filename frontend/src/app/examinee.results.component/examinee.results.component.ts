3165ce6cf986074465036b818685676e94d3392c
petrenko-a <p>
2024-04-04 13:05:51 +0500

    Добавил будущий компонент для отображения результатов тестирования

---
+++ frontend/src/app/examinee.results.component/examinee.results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'examinee-results-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './examinee.results.component.html',
+  styleUrl: './examinee.results.component.css',
+})
+export class ExamineeResultsComponent {}
