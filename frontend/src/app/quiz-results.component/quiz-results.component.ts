76e03fd65c8a6f3ccc5e8a223ed1107843d0deb6
petrenko-a <p>
2024-04-04 13:19:46 +0500

    Пока оставил. Но в будущем может быть удалён.

---
+++ frontend/src/app/quiz-results.component/quiz-results.component.ts
@@ -0,0 +1,19 @@
+import { Component, Injectable, Inject } from '@angular/core';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'quiz-results-component',
+  standalone: true,
+  templateUrl: './quiz-results.component.html',
+  imports: [MatDialogTitle, MatDialogContent, MatListModule],
+})
+@Injectable()
+export class QuizResultDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public items: string[]) {}
+}
+/*Пересмотреть компонент. Скорей всего стоит удалить*/
