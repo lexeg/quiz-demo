ec969dbd347959737f75c5b5a57639cfe79af58e
petrenko-a <p>
2024-04-04 13:12:29 +0500

    Добавил заготоку компонента для отображения детальной информации результатов прохождения теста.

---
+++ frontend/src/app/examinee.details.component/examinee.details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'examinee-details-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './examinee.details.component.html',
+  styleUrl: './examinee.details.component.css',
+})
+export class ExamineeDetailsComponent {}
