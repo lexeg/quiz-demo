87d8dae2bcf32e5aa24c7dc49f4dbab8a3880ae7
petrenko-a <p>
2024-04-10 13:59:03 +0500

    Добавил на главную страницу ссылки со страницами на результаты экзаменов и тесты

--- frontend/src/app/components/main-component/main-component.ts
+++ frontend/src/app/components/main-component/main-component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'main-component',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
