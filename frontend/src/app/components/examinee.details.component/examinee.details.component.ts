12e6e6aaa4da7683ca696637299d64277c0f732f
petrenko-a <p>
2024-04-10 13:12:31 +0500

    Добавил страничку с отображением деталей прохождения теста

--- frontend/src/app/components/examinee.details.component/examinee.details.component.ts
+++ frontend/src/app/components/examinee.details.component/examinee.details.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CandidatesService } from '../../services/candidates.service';
+import { CandidateResultResponse } from '../../contracts/CandidateResultResponse';
 
 @Component({
   selector: 'examinee-details-component',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    NgIf,
+  ],
   templateUrl: './examinee.details.component.html',
   styleUrl: './examinee.details.component.css',
+  providers: [CandidatesService],
 })
-export class ExamineeDetailsComponent {}
+export class ExamineeDetailsComponent implements OnInit {
+  isLoaded: boolean;
+  displayedColumns: string[] = [
+    'position',
+    'question',
+    'candidate-answer',
+    'answer',
+  ];
+  candidateResult: CandidateResultResponse;
+
+  constructor(private candidatesService: CandidatesService) {}
+
+  ngOnInit(): void {
+    this.loadQuestions();
+  }
+
+  private loadQuestions() {
+    this.candidatesService
+      .getResultsById('<fake-id>')
+      .subscribe((data: CandidateResultResponse) => {
+        this.candidateResult = data;
+        this.isLoaded = true;
+      });
+  }
+}
