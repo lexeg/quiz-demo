d1dbb7d622955d3b5f7565fe4746737e3210e17b
apetrenko <apetrenko@gseis.ru>
2024-05-03 10:04:17 +0500

    Добавил новый скрипт миграции.

--- scripts/quizdb.sql
+++ scripts/quizdb.sql
@@ -9,7 +9,30 @@ START TRANSACTION;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240330175624_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
+    CREATE TABLE branch_office_table (
+        id uuid NOT NULL,
+        name character varying(200) NOT NULL,
+        prefix character varying(10) NOT NULL,
+        CONSTRAINT branch_office_table_pkey PRIMARY KEY (id)
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
+    CREATE TABLE educational_program_table (
+        external_id uuid NOT NULL,
+        name character varying(100) NOT NULL,
+        CONSTRAINT educational_program_table_pkey PRIMARY KEY (external_id)
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     CREATE TABLE tests_table (
         id uuid NOT NULL,
         name character varying(150) NOT NULL,
@@ -21,7 +44,7 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240330175624_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     CREATE TABLE questions_table (
         id uuid NOT NULL,
         test_id uuid NOT NULL,
@@ -35,63 +58,57 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240330175624_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     CREATE TABLE test_results_table (
         id uuid NOT NULL,
         test_id uuid NOT NULL,
+        branch_office_id uuid NOT NULL,
+        educational_program_id uuid NOT NULL,
         email character varying(100) NOT NULL,
+        full_name character varying(100) NOT NULL,
+        mobile_phone character varying(20) NOT NULL,
         answers json NOT NULL,
         CONSTRAINT test_results_table_pkey PRIMARY KEY (id),
-        CONSTRAINT test_results_table_id_fkey FOREIGN KEY (id) REFERENCES tests_table (id)
+        CONSTRAINT test_results_table_branch_office_id_fkey FOREIGN KEY (branch_office_id) REFERENCES branch_office_table (id),
+        CONSTRAINT test_results_table_educational_program_id_fkey FOREIGN KEY (educational_program_id) REFERENCES educational_program_table (external_id),
+        CONSTRAINT test_results_table_id_fkey FOREIGN KEY (test_id) REFERENCES tests_table (id)
     );
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240330175624_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     CREATE INDEX "IX_questions_table_test_id" ON questions_table (test_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240330175624_InitialCreate') THEN
-    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-    VALUES ('20240330175624_InitialCreate', '8.0.3');
-    END IF;
-END $EF$;
-COMMIT;
-
-START TRANSACTION;
-
-
-DO $EF$
-BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240402084544_FixForeignKeyInTestResultsTable') THEN
-    ALTER TABLE test_results_table DROP CONSTRAINT test_results_table_id_fkey;
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
+    CREATE INDEX "IX_test_results_table_branch_office_id" ON test_results_table (branch_office_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240402084544_FixForeignKeyInTestResultsTable') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
+    CREATE INDEX "IX_test_results_table_educational_program_id" ON test_results_table (educational_program_id);
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     CREATE UNIQUE INDEX "IX_test_results_table_test_id" ON test_results_table (test_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240402084544_FixForeignKeyInTestResultsTable') THEN
-    ALTER TABLE test_results_table ADD CONSTRAINT test_results_table_id_fkey FOREIGN KEY (test_id) REFERENCES tests_table (id);
-    END IF;
-END $EF$;
-
-DO $EF$
-BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240402084544_FixForeignKeyInTestResultsTable') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503050054_InitialCreate') THEN
     INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-    VALUES ('20240402084544_FixForeignKeyInTestResultsTable', '8.0.3');
+    VALUES ('20240503050054_InitialCreate', '8.0.3');
     END IF;
 END $EF$;
 COMMIT;
