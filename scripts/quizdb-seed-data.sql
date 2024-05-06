f0fdb02213ab726d2c199817b84913c3521e8a95
petrenko-a <p>
2024-05-06 14:35:43 +0500

    поправил скрипты.

--- scripts/quizdb-seed-data.sql
+++ scripts/quizdb-seed-data.sql
@@ -10,14 +10,17 @@ VALUES ('2EAD9186-CEB4-4EE3-907F-5043F2A013FA', 'РПО'),
 INSERT INTO public.tests_table(id, name, description)
 VALUES ('d4553a73-0fcc-4055-97fb-683de215a878', 'Test №1', 'Описание для теста № 1');
 
+INSERT INTO public."PresignedUrls"("Id", "BranchOfficeId", "EducationalProgramId", "TestId", "PresignedUrl", "ExpiredDate")
+VALUES ('4400ECB2-5C00-42CA-940E-7A043624F1C8', '2952F21F-5FED-4AA8-BA77-C8A466230349', '2EAD9186-CEB4-4EE3-907F-5043F2A013FA', 'd4553a73-0fcc-4055-97fb-683de215a878', '3F6-_nVu3Ag', NOW() + INTERVAL '1 DAY');
+
 INSERT INTO public.questions_table(id, test_id, question, answers)
 VALUES ('67a8871c-1bc9-47e3-8e2c-504ee3c4253c', 'd4553a73-0fcc-4055-97fb-683de215a878', 'Вопрос 3', '{"Answers":[{"Id":1,"Text":"ответ-1 для вопроса 3"},{"Id":2,"Text":"ответ-2 для вопроса 3"},{"Id":3,"Text":"ответ-3 для вопроса 3"},{"Id":4,"Text":"ответ-4 для вопроса 3"}],"AnswerId":1}'),
 	   ('868c8a81-6afa-44ca-bb7a-4a399850576e', 'd4553a73-0fcc-4055-97fb-683de215a878', 'Вопрос 1', '{"Answers":[{"Id":1,"Text":"ответ-1 для вопроса 1"},{"Id":2,"Text":"ответ-2 для вопроса 1"},{"Id":3,"Text":"ответ-3 для вопроса 1"},{"Id":4,"Text":"ответ-4 для вопроса 1"}],"AnswerId":3}'),
 	   ('ae2073fc-2846-475d-83ff-9cad94613d6b', 'd4553a73-0fcc-4055-97fb-683de215a878', 'Вопрос 3', '{"Answers":[{"Id":1,"Text":"ответ-1 для вопроса 4"},{"Id":2,"Text":"ответ-2 для вопроса 4"},{"Id":3,"Text":"ответ-3 для вопроса 4"},{"Id":4,"Text":"ответ-4 для вопроса 4"}],"AnswerId":3}'),
 	   ('b27f73c5-1675-4aa4-b7a6-5fe6f1bfaec4', 'd4553a73-0fcc-4055-97fb-683de215a878', 'Вопрос 2', '{"Answers":[{"Id":1,"Text":"ответ-1 для вопроса 2"},{"Id":2,"Text":"ответ-2 для вопроса 2"},{"Id":3,"Text":"ответ-3 для вопроса 2"},{"Id":4,"Text":"ответ-4 для вопроса 2"}],"AnswerId":2}'),
 	   ('f990629f-dd01-4f2e-a6d8-3cfc6ace9229', 'd4553a73-0fcc-4055-97fb-683de215a878', 'Вопрос 5', '{"Answers":[{"Id":1,"Text":"ответ-1 для вопроса 5"},{"Id":2,"Text":"ответ-2 для вопроса 5"},{"Id":3,"Text":"ответ-3 для вопроса 5"},{"Id":4,"Text":"ответ-4 для вопроса 5"}],"AnswerId":4}');
-	   
-INSERT INTO public.test_results_table(id, test_id, branch_office_id, educational_program_id, email, full_name, mobile_phone, presigned_url, expired_date, answers)
+
+INSERT INTO public.test_results_table(id, test_id, branch_office_id, educational_program_id, email, full_name, mobile_phone, answers)
 VALUES ('ce86b0f1-0550-40e0-947a-b22124f1246a',
 		'd4553a73-0fcc-4055-97fb-683de215a878',
 		'2952F21F-5FED-4AA8-BA77-C8A466230349',
@@ -25,6 +28,4 @@ VALUES ('ce86b0f1-0550-40e0-947a-b22124f1246a',
 		'fedya@gmail.com',
 		'Фёдоров Фёдор Фёдорович',
 		'+7(999)123-45-67',
-		'e2fd2fda9b8e9b10fc3bc4c027ecb913',
-		NOW() + INTERVAL '1 DAY',
 		'[{"QuestionId":"67a8871c-1bc9-47e3-8e2c-504ee3c4253c","AnswerId":2},{"QuestionId":"868c8a81-6afa-44ca-bb7a-4a399850576e","AnswerId":3},{"QuestionId":"ae2073fc-2846-475d-83ff-9cad94613d6b","AnswerId":3},{"QuestionId":"b27f73c5-1675-4aa4-b7a6-5fe6f1bfaec4","AnswerId":2},{"QuestionId":"f990629f-dd01-4f2e-a6d8-3cfc6ace9229","AnswerId":1}]');
--- scripts/quizdb.sql
+++ scripts/quizdb.sql
@@ -9,7 +9,7 @@ START TRANSACTION;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE TABLE branch_office_table (
         id uuid NOT NULL,
         name character varying(200) NOT NULL,
@@ -21,7 +21,7 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE TABLE educational_program_table (
         external_id uuid NOT NULL,
         name character varying(100) NOT NULL,
@@ -32,7 +32,7 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE TABLE tests_table (
         id uuid NOT NULL,
         name character varying(150) NOT NULL,
@@ -44,7 +44,25 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
+    CREATE TABLE "PresignedUrls" (
+        "Id" uuid NOT NULL,
+        "BranchOfficeId" uuid NOT NULL,
+        "EducationalProgramId" uuid NOT NULL,
+        "TestId" uuid NOT NULL,
+        "PresignedUrl" text,
+        "ExpiredDate" timestamp with time zone NOT NULL,
+        CONSTRAINT "PK_PresignedUrls" PRIMARY KEY ("Id"),
+        CONSTRAINT "FK_PresignedUrls_branch_office_table_BranchOfficeId" FOREIGN KEY ("BranchOfficeId") REFERENCES branch_office_table (id) ON DELETE CASCADE,
+        CONSTRAINT "FK_PresignedUrls_educational_program_table_EducationalProgramId" FOREIGN KEY ("EducationalProgramId") REFERENCES educational_program_table (external_id) ON DELETE CASCADE,
+        CONSTRAINT "FK_PresignedUrls_tests_table_TestId" FOREIGN KEY ("TestId") REFERENCES tests_table (id) ON DELETE CASCADE
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE TABLE questions_table (
         id uuid NOT NULL,
         test_id uuid NOT NULL,
@@ -58,7 +76,7 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE TABLE test_results_table (
         id uuid NOT NULL,
         test_id uuid NOT NULL,
@@ -67,8 +85,6 @@ BEGIN
         email character varying(100) NOT NULL,
         full_name character varying(100) NOT NULL,
         mobile_phone character varying(20) NOT NULL,
-        presigned_url character varying(300) NOT NULL,
-        expired_date timestamp with time zone NOT NULL,
         answers json NOT NULL,
         CONSTRAINT test_results_table_pkey PRIMARY KEY (id),
         CONSTRAINT test_results_table_branch_office_id_fkey FOREIGN KEY (branch_office_id) REFERENCES branch_office_table (id),
@@ -80,37 +96,58 @@ END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
+    CREATE INDEX "IX_PresignedUrls_BranchOfficeId" ON "PresignedUrls" ("BranchOfficeId");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
+    CREATE INDEX "IX_PresignedUrls_EducationalProgramId" ON "PresignedUrls" ("EducationalProgramId");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
+    CREATE INDEX "IX_PresignedUrls_TestId" ON "PresignedUrls" ("TestId");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE INDEX "IX_questions_table_test_id" ON questions_table (test_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE INDEX "IX_test_results_table_branch_office_id" ON test_results_table (branch_office_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE INDEX "IX_test_results_table_educational_program_id" ON test_results_table (educational_program_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     CREATE UNIQUE INDEX "IX_test_results_table_test_id" ON test_results_table (test_id);
     END IF;
 END $EF$;
 
 DO $EF$
 BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240503151741_InitialCreate') THEN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240506060454_InitialCreate') THEN
     INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-    VALUES ('20240503151741_InitialCreate', '8.0.3');
+    VALUES ('20240506060454_InitialCreate', '8.0.3');
     END IF;
 END $EF$;
 COMMIT;
